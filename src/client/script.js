974d11c46a7d5a1e19662e91f58e358469a4f2a2
1985lxw <150556638+1985lxw@users.noreply.github.com>
2024-04-23 15:17:58 -0400

    delete some stuff

--- src/client/script.js
+++ src/client/script.js
@@ -1,13 +1,11 @@
 const toggleSwitch = document.getElementById("switch");
 const title = document.getElementById("title");
-const icon = document.getElementById("bx-transfer");
 const inputTextbox = document.getElementById("input-textbox");
 const outputTextbox = document.getElementById("output-textbox");
 
 toggleSwitch.addEventListener('click', () => {
   document.body.style.backgroundColor = document.body.style.backgroundColor === 'pink' ? 'black' : 'pink';
   title.style.color = document.body.style.backgroundColor === 'black' ? 'pink' : 'black';
-  icon.color = document.body.style.backgroundColor === 'black' ? 'pink' : 'black';
 });
 
 inputTextbox.addEventListener("input", ()=>{
