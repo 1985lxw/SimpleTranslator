dfae07f46649228655db29b61e2ba22ce441e063
1985lxw <150556638+1985lxw@users.noreply.github.com>
2024-04-23 14:03:56 -0400

    switch languages icon

--- src/client/script.js
+++ src/client/script.js
@@ -1,11 +1,13 @@
 const toggleSwitch = document.getElementById("switch");
 const title = document.getElementById("title");
+const icon = document.getElementById("trans");
 const inputTextbox = document.getElementById("input-textbox");
 const outputTextbox = document.getElementById("output-textbox");
 
 toggleSwitch.addEventListener('click', () => {
   document.body.style.backgroundColor = document.body.style.backgroundColor === 'pink' ? 'black' : 'pink';
   title.style.color = document.body.style.backgroundColor === 'black' ? 'pink' : 'black';
+  icon.color = document.body.style.backgroundColor === 'black' ? 'pink' : 'black';
 });
 
 inputTextbox.addEventListener("input", ()=>{
