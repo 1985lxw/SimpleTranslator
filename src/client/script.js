081d1a332a200784945bddb9a74cb7f78dfc0639
1985lxw <150556638+1985lxw@users.noreply.github.com>
2024-04-23 12:30:56 -0400

    styling

--- src/client/script.js
+++ src/client/script.js
@@ -1,5 +1,7 @@
 const toggleSwitch = document.getElementById("switch");
+const title = document.getElementById("title");
 
 toggleSwitch.addEventListener('click', () => {
   document.body.style.backgroundColor = document.body.style.backgroundColor === 'pink' ? 'black' : 'pink';
+  title.style.color = document.body.style.backgroundColor === 'black' ? 'pink' : 'black';
 });
