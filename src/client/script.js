cb07ab4ed6c473d8d8b2aa8cdca3f5235516e684
1985lxw <150556638+1985lxw@users.noreply.github.com>
2024-04-23 13:09:35 -0400

    Update script.js

--- src/client/script.js
+++ src/client/script.js
@@ -1,15 +1,12 @@
 const toggleSwitch = document.getElementById("switch");
+const title = document.getElementById("title");
 const inputTextbox = document.getElementById("input-textbox");
 const outputTextbox = document.getElementById("output-textbox");
 
 toggleSwitch.addEventListener('click', () => {
-  // Toggle background color between pink and black
   document.body.style.backgroundColor = document.body.style.backgroundColor === 'pink' ? 'black' : 'pink';
+  title.style.color = document.body.style.backgroundColor === 'black' ? 'pink' : 'black';
 });
-// toggleSwitch.addEventListener('click', () => {
-//     const label = document.querySelector('label');
-//     label.textContent = label.textContent === '🎀' ? '💀' : '🎀';
-//   });
 
 inputTextbox.addEventListener("input", ()=>{
   outputTextbox.value = inputTextbox.value;
